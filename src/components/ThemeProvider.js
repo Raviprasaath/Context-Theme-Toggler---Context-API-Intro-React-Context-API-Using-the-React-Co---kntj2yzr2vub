80f27b465a7cd4e3d749015b02887785513823d5
Raviprasaath Ramesh <117162868+Raviprasaath@users.noreply.github.com>
2023-09-27 07:19:20 +0530

    Update ThemeProvider.js

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 const ThemeContext = React.createContext()
 const ThemeProvider = (props) =>{
+    const [theme, setTheme] = useState("light");
 
+    const toggleTheme = () => {
+        if (theme === "light") {
+            setTheme("dark");
+        } else {
+            setTheme("light")
+        }
+    }
+
+    const initalState = {
+        theme, toggleTheme,
+    }
+    
     return (
         <React.Fragment>
-
+          <ThemeContext.Provider value={initialState}>
+            {props.children}
+          </ThemeContext.Provider>
         </React.Fragment>
     )
 }
